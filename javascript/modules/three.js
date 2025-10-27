24d081f960b3571284da3aa809d3accbddd3ad46
yitsian <tsian@jgong.com>
2025-10-26 21:50:34 -0600

    uhhh three brokey

--- javascript/modules/three.js
+++ javascript/modules/three.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const camera = new THREE.PerspectiveCamera(75, 300 / 200, 0.1, 1000);
 
